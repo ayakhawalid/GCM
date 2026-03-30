2cccdec6dda5180c853bc416fb0d52bc54192ff4
JCS1029 <lovely.m.2004@gmail.com>
2026-03-30 15:57:22 +0300

    cleaned the project and fixed jar files.

--- migration_tour_distance.sql
+++
@@ -1,4 +0,0 @@
--- Add total_distance_meters to tours (sum of consecutive POI-to-POI distances for the tour route).
-USE gcm_db;
-
-ALTER TABLE tours ADD COLUMN total_distance_meters DOUBLE NULL AFTER estimated_duration_minutes;
--- migration_tour_maps.sql
+++
@@ -1,5 +0,0 @@
--- Add tour_id to maps so a map can be the dedicated "tour route" map for a tour.
-USE gcm_db;
-
-ALTER TABLE maps ADD COLUMN tour_id INT NULL;
-ALTER TABLE maps ADD CONSTRAINT fk_maps_tour FOREIGN KEY (tour_id) REFERENCES tours(id) ON DELETE CASCADE;
